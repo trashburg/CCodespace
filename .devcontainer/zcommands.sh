30107fc128e4cfb51789165ca38207413736c7d5
trashburg <99060219+trashburg@users.noreply.github.com>
2022-09-20 15:36:18 +0000

    initalization

---
+++ .devcontainer/zcommands.sh
@@ -0,0 +1,14 @@
+function rebuild() {
+    rm -rf $WORKSPACE_FOLDER/build
+    mkdir $WORKSPACE_FOLDER/build
+    cmake -DCMAKE_BUILD_TYPE=Debug -S $WORKSPACE_FOLDER -B $WORKSPACE_FOLDER/build
+    cmake --build $WORKSPACE_FOLDER/build
+}
+
+function checkmem() {
+    valgrind --tool=memcheck --leak-check=yes --track-origins=yes $WORKSPACE_FOLDER/build/$APP_NAME
+}
+
+function runapp() {
+    $WORKSPACE_FOLDER/build/$APP_NAME
+}
